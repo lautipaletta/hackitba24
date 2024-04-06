cd884e35cbb94678686fbcac0b3b61e3b02d437b
Acuzito55 <asbsola@gmail.com>
2024-04-05 23:33:54 -0300

    feat: Schemas completed, juan is only user

--- server/server.js
+++ server/server.js
@@ -1,7 +1,46 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express(); 
 const PORT = 3000; 
+
+app.listen(PORT, (error) =>{ 
+    if(!error) 
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
+    else 
+        console.log("Error occurred, server can't start", error); 
+    } 
+);
+
+mongoose.connect('mongodb://127.0.0.1:27017/database');
+console.log("connected to database")
+
+const session = new mongoose.Schema({
+    text: [String],
+    summary: String,
+    timestamp: Number
+});
+
+const Session = mongoose.model("Session", session);
+
+const user = new mongoose.Schema({
+    name: String,
+    sessions: [session]
+});
+
+const User = mongoose.model("User", user);
+
+const database = new mongoose.Schema({
+    users: [user]
+});
+
+const Database = mongoose.model("Database", database);
+
+// Juan
+const juan = new User({_id: "1234", name:"juan", sessions: []});
+
+const db = new Database({users:[juan]});
+console.log(db);
 
 app.get('/start_session', (req, res)=>{ 
     console.log(req);
@@ -22,11 +61,3 @@ app.get('/message', (req, res)=>{
     console.log(req);
     res.status(200).send("hello");
 });
-
-app.listen(PORT, (error) =>{ 
-    if(!error) 
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
-    else 
-        console.log("Error occurred, server can't start", error); 
-    } 
-);
