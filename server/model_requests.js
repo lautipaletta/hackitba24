e04f4dad0454d32b97877e24c415aff45924f3c6
Ignacio Searles <nachosearles3@gmail.com>
2024-04-06 00:24:51 -0300

    feat: system prompts created and tested

--- server/model_requests.js
+++ server/model_requests.js
@@ -8,7 +8,7 @@ const Mauro_system_prompt = fs.readFileSync("./prompts/Mauro_system_prompt.txt")
 
 function message_list_to_conversation_list(messages) {
     return messages.map((message) => {
-        const out = {role: (!message.user_id) ? "system" : "user", content: message.content};
+        const out = {role: (!message.user_id) ? "assistant" : "user", content: message.content};
         return out;
     });
 }
@@ -17,7 +17,7 @@ function message_list_to_conversation_string(messages) {
     const conversation = "";
 
     messages.forEach((message) => {
-        conversation += `${(!message.user_id) ? "ALICIA" : "USER"}: ${message.content}`;
+        conversation += `${(!message.user_id) ? "ALICIA" : "USUARIO"}: ${message.content}`;
     });
 
     return conversation;
@@ -25,7 +25,7 @@ function message_list_to_conversation_string(messages) {
 
 async function get_next_message_from_Alicia(messages, prev_summary) {
     const messages_as_conversation = message_list_to_conversation_list(messages);
-    const system_prompt = Alicia_base_system_prompt + prev_summary;
+    const system_prompt = Alicia_base_system_prompt + prev_summary ? `Ten en cuenta el siguiente resumen de la última conversación que tuviste con el usuario:\n${prev_summary}` : "";
 
     const completion = await openai.chat.completions.create({
         messages: [{role: "system", content: system_prompt}, ...messages_as_conversation],
@@ -39,7 +39,7 @@ async function get_session_summary_from_Mauro(messages) {
     const messages_as_conversation_string = message_list_to_conversation_string(messages);
 
     const completion = await openai.chat.completions.create({
-        messages: [{role: "system", content: Mauro_system_prompt}, {role: "user", content: "messages_as_conversation_string"}],
+        messages: [{role: "system", content: Mauro_system_prompt}, {role: "user", content: messages_as_conversation_string}],
         model: "gpt-3.5-turbo"
     })
 
